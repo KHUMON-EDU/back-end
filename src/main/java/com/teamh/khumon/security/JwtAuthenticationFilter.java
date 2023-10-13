cca02179c3798c1abb3683a6ee2090200abf303c
Martin0o0 <8546088@naver.com>
2023-10-13 11:03:53 +0900

    feat: JWT를 발급하는 기능을 구현

---
+++ src/main/java/com/teamh/khumon/security/JwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.teamh.khumon.security;
+
+
+import com.teamh.khumon.repository.MemberRepository;
+import com.teamh.khumon.service.JwtProviderService;
+
+import com.teamh.khumon.service.MemberDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.io.IOException;
+import java.util.Optional;
+
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtProviderService jwtProviderService;
+
+    private final MemberDetailsService memberDetailsService;
+
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        // 예외 처리 : 들어온 토큰 값이 올바르지 않은 경우 다른 체인으로 넘어감.
+        final Optional<String> jwt = jwtProviderService.extractAccessToken(request);
+
+        // 재인증하지 않기 위해 사용자가 인증되었는지 확인.
+        // 로그인은 되어있고 아직 인증은 안된 경우만 if문 내부 접근
+        log.info("token 값 유효성 체크 시작 토큰 : " + jwt);
+        if (jwt.isPresent() && SecurityContextHolder.getContext().getAuthentication() == null
+                && jwtProviderService.validateToken(jwt.get())) {
+            String OAuth2Id = jwtProviderService.extractOAuth2Id(jwt.get());
+            UserDetails userDetails = memberDetailsService.loadUserByUsername(OAuth2Id);
+            Authentication authentication = jwtProviderService.getAuthentication(userDetails); //Authentication 객체 생성
+
+            //SecurityContextHolder에 새로운 SecurityContext 영역을 할당하고,
+            // 생성한 Authentication 객체를 담는다.
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authentication);
+            SecurityContextHolder.setContext(securityContext);
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
---
+++ src/main/java/com/teamh/khumon/service/JwtProviderService.java
@@ -0,0 +1,102 @@
+package com.teamh.khumon.service;
+
+import io.jsonwebtoken.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class JwtProviderService {
+
+    @Value("${jwt.secret}")
+    private String secret_key;
+
+
+    @Value("${jwt.access.expiration}")
+    private Long ACCESS_TOKEN_EXPIRATION_PERIOD;
+
+
+
+    @Value("${jwt.access.header}")
+    private String ACCESS_HEADER_KEY;
+
+
+
+    private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @PostConstruct
+    protected void init() {
+        log.info("secret_key Base64 인코딩시작");
+        log.info("Original Secret_Key : " + secret_key);
+        this.secret_key = Base64.getEncoder().encodeToString(secret_key.getBytes(StandardCharsets.UTF_8));
+        log.info("Encoded Base64 Secret_Key : " + secret_key);
+        log.info("secretKey 초기화 완료");
+    }
+
+    public String extractOAuth2Id(String jwt) {
+        return Jwts.parser().setSigningKey(secret_key).parseClaimsJws(jwt).getBody().getSubject();
+    }
+
+    public Optional<String> extractAccessToken(HttpServletRequest request) throws IOException, ServletException {
+        return Optional.ofNullable(request.getHeader(ACCESS_HEADER_KEY));
+    }
+
+
+    public Long getExpireTime(String token) {
+        Date expirationDate =  Jwts.parser().setSigningKey(secret_key).parseClaimsJws(token).getBody().getExpiration();
+        long now = new Date().getTime();
+        return ((expirationDate.getTime() - now) % 1000) + 1;
+    }
+
+
+
+    public String generateAccessToken(String OAuth2Id, String role) {
+        Claims claims = Jwts.claims().setSubject(OAuth2Id);
+        claims.put("role", role);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION_PERIOD))
+                .signWith(SignatureAlgorithm.HS256, secret_key)
+                .compact(); //토큰생성
+    }
+
+    public Authentication getAuthentication(UserDetails userDetails) {
+        log.info("토큰 인증 정보 조회 시작");
+        log.info("UserDetails UserName : {}",
+                userDetails.getUsername());
+        return new UsernamePasswordAuthenticationToken(userDetails, null,
+                authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
+    }
+
+
+    public boolean validateToken(String token) { //토큰 발리데이션
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret_key).parseClaimsJws(token);
+            log.info("토큰 유효 체크 완료");
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            log.info("JWT 토큰이 이상합니다.");
+            return false;
+        }
+    }
+
+}
