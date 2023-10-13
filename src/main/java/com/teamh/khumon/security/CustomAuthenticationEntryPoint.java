0e544041fc4ac5bda09a6006b89ff1a383b48ac5
Martin0o0 <8546088@naver.com>
2023-10-13 23:45:52 +0900

    feat: 인증 실패 핸들러 구현

---
+++ src/main/java/com/teamh/khumon/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package com.teamh.khumon.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        log.info("인증 실패");
+        try {
+            setResponse(response);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException, JSONException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject responseJson = new JSONObject();
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        responseJson.put("message", "인증이 실패했습니다.");
+        response.getWriter().write(objectMapper.writeValueAsString(responseJson));
+    }
+}
