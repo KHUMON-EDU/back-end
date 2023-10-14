3697b877ed0388be932bcf141ab479a4b7ee8b22
Martin0o0 <8546088@naver.com>
2023-10-14 13:54:48 +0900

    feat: 스프링 MVC CORS 설정

---
+++ src/main/java/com/teamh/khumon/configuration/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.teamh.khumon.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedHeaders("authorization", "User-Agent", "Cache-Control", "Content-Type")
+                .exposedHeaders("authorization", "User-Agent", "Cache-Control", "Content-Type")
+                .allowedMethods("*");
+    }
+}
