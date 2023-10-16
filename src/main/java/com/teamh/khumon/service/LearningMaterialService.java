8163517bbba5015516721996c2789f513918ea4b
June <pl0901@khu.ac.kr>
2023-10-16 20:26:48 +0900

    feat: 강의자료 업로드 로직 구현

---
+++ src/main/java/com/teamh/khumon/service/LearningMaterialService.java
@@ -0,0 +1,56 @@
+package com.teamh.khumon.service;
+
+
+import com.teamh.khumon.domain.LearningMaterial;
+import com.teamh.khumon.domain.Member;
+import com.teamh.khumon.dto.LearningRequest;
+import com.teamh.khumon.repository.LearningMaterialRepository;
+import com.teamh.khumon.repository.MemberRepository;
+import com.teamh.khumon.util.MediaUtil;
+import com.teamh.khumon.util.ObjectToDtoUtil;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class LearningMaterialService {
+
+
+    private final MemberRepository memberRepository;
+
+    private final LearningMaterialRepository learningMaterialRepository;
+
+    private final MediaUtil mediaUtil;
+
+
+    @Transactional
+    public ResponseEntity<?> createLearningMaterial(Principal principal, MultipartFile multipartFile, String data) {
+        Member member = memberRepository.findByUsername(principal.getName()).orElseThrow();
+        try {
+            LearningRequest learningRequest = (LearningRequest) new ObjectToDtoUtil().jsonStrToObj(data, LearningRequest.class);
+            LearningMaterial learningMaterial = LearningMaterial.builder()
+                    .title(learningRequest.getTitle())
+                    .content(learningRequest.getContent())
+                    .member(member)
+                    .build();
+            String uploadedFileUrl = mediaUtil.uploadMaterial(multipartFile);
+            learningMaterial.setFileName(multipartFile.getOriginalFilename());
+            learningMaterial.setFileURL(uploadedFileUrl);
+            learningMaterial.setMediaFileType(mediaUtil.findMediaType(multipartFile.getName()));
+            Long id = learningMaterialRepository.save(learningMaterial).getId();
+            Map<String, Long> response = new HashMap<>();
+            response.put("id", id);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
