bb6ea4f6d82b0eac0ef748f6872f6506fbfa7855
Martin0o0 <8546088@naver.com>
2023-10-17 12:56:49 +0900

    fix: 파일 저장 로직 수정

--- src/main/java/com/teamh/khumon/util/MediaUtil.java
+++ src/main/java/com/teamh/khumon/util/MediaUtil.java
@@ -32,11 +32,12 @@ public class MediaUtil {
             String generateFileName;
             generateFileName = UUID.randomUUID().toString() + "." + extension;
             String mediaURL =  File.separator + generateFileName;
-            String destinationPath = MAIN_DIR_NAME +  SUB_DIR_NAME + mediaURL;
+            String destinationPath = folder.getPath() + mediaURL;
             File destination = new File(destinationPath);
             media.transferTo(destination);
 
-            return mediaURL;
+
+            return SUB_DIR_NAME + mediaURL;
         } catch (Exception e) {
             throw new Exception("강의자료 형식이 아님");
         }
@@ -49,14 +50,18 @@ public class MediaUtil {
     public MediaFileType findMediaType(String fileName) {
 
         String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-
+        log.info("extension : " + extension);
         if (extension.equals("txt")) {
+            log.info("txt");
             return MediaFileType.TEXT;
         } else if(extension.equals("pdf")){
+            log.info("pdf");
             return MediaFileType.PDF;
         } else if(extension.equals("mp4") || extension.equals("mpeg") || extension.equals("mov") || extension.equals("mkv") || extension.equals("avi") || extension.equals("wmv")){
+            log.info("video");
             return MediaFileType.VIDEO;
         }else{
+            log.info("etc");
             return MediaFileType.ETC;
         }
     }
