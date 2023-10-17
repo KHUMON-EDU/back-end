900079a0d516e6b87c420e03e3f97697ec845985
Martin0o0 <8546088@naver.com>
2023-10-17 11:49:53 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/teamh/khumon/controller/AuthenticationController.java
+++ src/main/java/com/teamh/khumon/controller/AuthenticationController.java
@@ -1,4 +1,32 @@
 package com.teamh.khumon.controller;
 
+import com.teamh.khumon.service.AuthenticationService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController {
+
+
+    private final AuthenticationService authenticationService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        authenticationService.logout(httpServletRequest, httpServletResponse);
+        return ResponseEntity.ok().build();
+    }
 }
