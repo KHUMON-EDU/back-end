7032dfa8df183b079b35bd797d076d37f007511b
June <pl0901@khu.ac.kr>
2023-10-18 19:40:41 +0900

    feat: 학습 자료 상제 정보 엔드포인트 생성

--- src/main/java/com/teamh/khumon/controller/LearningMaterialController.java
+++ src/main/java/com/teamh/khumon/controller/LearningMaterialController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,10 +20,16 @@ public class LearningMaterialController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping(value ="/learning-material", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<?> createRoom(@RequestPart(value = "media", required= true) MultipartFile multipartFile,
-                                        @RequestPart(value = "data", required = true) String data,
-                                        Principal principal) throws IOException {
+    public ResponseEntity<?> createLearningMaterial(@RequestPart(value = "media", required= true) MultipartFile multipartFile,
+                                                    @RequestPart(value = "data", required = true) String data,
+                                                    Principal principal) throws IOException {
         return learningMaterialService.createLearningMaterial(principal, multipartFile, data);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/learning-material/{learningMaterialId}")
+    public ResponseEntity<?> getLearningMaterial(@PathVariable(name = "learningMaterialId") Long id, Principal principal){
+        return learningMaterialService.getLearningMaterial(id, principal);
     }
 
 
