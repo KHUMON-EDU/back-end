4f68f9b9ce417c3d128fb40aa2624e8befc8ade4
June <pl0901@khu.ac.kr>
2023-10-31 12:26:09 +0900

    feat: Offset Pagination Dto 생성

---
+++ src/main/java/com/teamh/khumon/dto/OffsetPagination.java
@@ -0,0 +1,24 @@
+package com.teamh.khumon.dto;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OffsetPagination {
+    private List<LearningMaterialContent> content;
+    private Long totalElements;
+    private Integer totalPages;
+    private Integer size;
+    private Integer pageNumber;
+    private Integer numberOfElements;
+    private Boolean isFirst;
+    private Boolean isLast;
+    private Boolean isEmpty;
+}
