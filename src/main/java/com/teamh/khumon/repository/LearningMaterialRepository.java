6fe147208e7913b208265ac75affccf036dda6b1
June <pl0901@khu.ac.kr>
2023-10-31 12:29:01 +0900

    feat: repository의 키워드 검색 쿼리 메서드 생성

--- src/main/java/com/teamh/khumon/repository/LearningMaterialRepository.java
+++ src/main/java/com/teamh/khumon/repository/LearningMaterialRepository.java
@@ -1,7 +1,11 @@
 package com.teamh.khumon.repository;
 
 import com.teamh.khumon.domain.LearningMaterial;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LearningMaterialRepository extends JpaRepository<LearningMaterial, Long> {
+
+    Page<LearningMaterial> findByMemberIdAndTitleContainingOrContentContaining(Long memberId, String title, String content, Pageable pageable);
 }
